a5a5bdbd8f11effbc4c3810016bb89aea04213e7
Amrutha2204 <nallaamruthareddy1@gmail.com>
2026-01-21 14:09:34 +0530

    Register API

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -2,6 +2,23 @@ import axios from "axios";
 
 const API = "http://localhost:8001/api/v1/auth";
 
+/* REGISTER */
+export const registerUser = (data: {
+  name: string;
+  email: string;
+  password: string;
+  phoneNumber: string;
+  timezone: string;
+  roleId: number;
+  guestId: string | null;
+  dob: string;
+  birthPlace: string;
+  birthTime: string;
+}) => {
+  return axios.post(`${API}/signup`, data);
+};
+
+/* LOGIN */
 export const loginUser = (data: {
   email: string;
   password: string;
